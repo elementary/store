0f1e5ed4de1c25f8036b5cb420c037e163facce2
Blake Kostner <git@btkostner.io>
2020-10-03 12:38:51 -0600

    fix: disable the NProgress spinner

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,6 +8,8 @@ import { LiveSocket } from 'phoenix_live_view'
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
 const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
 
+NProgress.configure({ showSpinner: false })
+
 window.addEventListener('phx:page-loading-start', info => NProgress.start())
 window.addEventListener('phx:page-loading-stop', info => NProgress.done())
 
