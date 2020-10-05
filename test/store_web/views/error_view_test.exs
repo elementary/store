d5842528cbf07f585295e59d16b5d34a73001093
Blake Kostner <git@btkostner.io>
2020-10-05 16:59:02 -0600

    fix: update error view tests with new html templates

--- test/store_web/views/error_view_test.exs
+++ test/store_web/views/error_view_test.exs
@@ -5,11 +5,11 @@ defmodule Elementary.StoreWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(Elementary.StoreWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(Elementary.StoreWeb.ErrorView, "404.html", []) =~ "Not Found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(Elementary.StoreWeb.ErrorView, "500.html", []) ==
+    assert render_to_string(Elementary.StoreWeb.ErrorView, "500.html", []) =~
              "Internal Server Error"
   end
 end
