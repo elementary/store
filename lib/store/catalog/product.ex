cbcfbc05083f858cce4d0b9c708e8ecf68fa7431
Blake Kostner <git@btkostner.io>
2021-01-30 19:38:03 -0700

    add starting price to homepage

--- lib/store/catalog/product.ex
+++ lib/store/catalog/product.ex
@@ -7,18 +7,26 @@ defmodule Elementary.Store.Catalog.Product do
 
   @enforce_keys [:id]
 
-  defstruct [:id, :name, :description, :category, :thumbnail_url, :variants]
+  defstruct [:id, :name, :description, :category, :price_range, :thumbnail_url, :variants]
 
   @doc """
   Converts some Printful API data to an `Elementary.Store.Catalog.Product`
   struct. This includes consolidating some resources, and renaming some fields.
   """
   def from_printful(product, catalog) do
+    price_range =
+      product
+      |> Map.get(:sync_variants, [])
+      |> Enum.map(&Map.get(&1, :retail_price, "0"))
+      |> Enum.map(&String.to_float/1)
+      |> Enum.sort()
+
     struct(__MODULE__,
       id: product.sync_product.id,
       name: product.sync_product.name,
       description: catalog.product.description,
       category: Category.from_printful(catalog),
+      price_range: [hd(price_range), List.last(price_range)],
       thumbnail_url: product.sync_product.thumbnail_url
     )
   end
