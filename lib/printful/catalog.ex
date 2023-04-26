94bc46fd7c94e8f6a429c242790bc80e47b72417
David Hewitt <davidmhewitt@gmail.com>
2023-04-27 00:43:51 +0100

    printful: Gracefully handle missing variants

--- lib/printful/catalog.ex
+++ lib/printful/catalog.ex
@@ -3,9 +3,13 @@ defmodule Printful.Catalog do
   Handles everything catalog related for Printful.
   """
 
-  alias Printful.Api
+  alias Printful.{Api, ApiError}
 
   def variant(id) do
-    Api.get("/products/variant/" <> to_string(id))
+    try do
+      {:ok, Api.get("/products/variant/" <> to_string(id))}
+    rescue
+      e in ApiError -> {:error, e}
+    end
   end
 end
--- lib/store/catalog/catalog.ex
+++ lib/store/catalog/catalog.ex
@@ -20,6 +20,8 @@ defmodule Elementary.Store.Catalog do
 
     products
     |> Enum.zip(variants)
+    |> Enum.filter(fn {_a, b} -> elem(b, 0) == :ok end)
+    |> Enum.map(fn {a, b} -> {a, elem(b, 1)} end)
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.map(&apply(Product, :from_printful, &1))
   end
@@ -30,11 +32,19 @@ defmodule Elementary.Store.Catalog do
     catalog_variant_id = hd(product.sync_variants).product.variant_id
     catalog = Printful.Catalog.variant(catalog_variant_id)
 
+    if elem(catalog, 0) != :ok do
+      throw(elem(catalog, 1))
+    end
+
+    catalog = elem(catalog, 1)
+
     variants =
       product
       |> Map.get(:sync_variants)
       |> Enum.map(&Map.get(&1, :id))
       |> Enum.map(&get_variant/1)
+      |> Enum.filter(fn {a, _b} -> a == :ok end)
+      |> Enum.map(fn {_a, b} -> b end)
 
     product
     |> Product.from_printful(catalog)
@@ -46,12 +56,16 @@ defmodule Elementary.Store.Catalog do
     |> Map.get(:sync_variants)
     |> Enum.map(&Map.get(&1, :id))
     |> Enum.map(&get_variant/1)
+    |> Enum.filter(fn {a, _b} -> a == :ok end)
+    |> Enum.map(fn {_a, b} -> b end)
   end
 
   def get_variant(variant_id) do
     store = Printful.Store.variant(variant_id)
-    catalog = Printful.Catalog.variant(store.product.variant_id)
 
-    Variant.from_printful(store, catalog)
+    case Printful.Catalog.variant(store.product.variant_id) do
+      {:ok, catalog} -> {:ok, Variant.from_printful(store, catalog)}
+      {:error, reason} -> {:error, reason}
+    end
   end
 end
