5d2389691c4130bddc74d0646204801f6e83c4de
Corentin Noël <corentin.noel@collabora.com>
2025-06-04 10:04:59 +0200

    Update elixir requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elementary.Store.MixProject do
     [
       app: :store,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
